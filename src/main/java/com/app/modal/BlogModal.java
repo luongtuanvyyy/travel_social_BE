c351ae8bfe44fa43bf36f7f697854129a1a3c733
luongtuanvyyy <vyltps22007@fpt.edu.vn>
2023-12-15 11:04:28 +0700

    change type date respone

--- src/main/java/com/app/modal/BlogModal.java
+++ src/main/java/com/app/modal/BlogModal.java
@@ -1,20 +1,28 @@
 package com.app.modal;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Date;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class BlogModal {
     private int id;
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private Date createdAt;
     private String createdBy;
     private boolean isActivated;
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private Date modifiedAt;
     private String modifiedBy;
     private String cloudinaryId;
